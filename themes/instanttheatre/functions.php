d35ebab4998ae985ca26d8c83bef23cf5471fe66
Guy Chappell <mettcat@gmail.com>
2018-03-15 14:27:19 -0700

    Enqueue isotope jQuery plugin

--- themes/instanttheatre/functions.php
+++ themes/instanttheatre/functions.php
@@ -107,7 +107,8 @@ function red_starter_scripts() {
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 	wp_enqueue_script( 'instant.js', get_template_directory_uri() . '/build/js/instant.min.js', array(), '20130115', true );
 	wp_enqueue_script( 'jquery' );	
-	wp_enqueue_script( 'spectragram.js', './js/spectragram.js' );
+	wp_enqueue_script( 'spectragram.js', './js/lib/spectragram.js' );
+	wp_enqueue_script( 'isotope', './js/lib/isotope.pkgd.min.js' );
 	
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
