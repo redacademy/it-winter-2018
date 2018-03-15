cd6e4e176d4ed85b250d112f8a894a1cb9015330
Guy Chappell <mettcat@gmail.com>
2018-03-15 14:13:06 -0700

    Added Role taxonomy to Person CPT. Layout & Styling started on taxonomy-role.php. Added isotope jQuery plugin.

--- themes/instanttheatre/header.php
+++ themes/instanttheatre/header.php
@@ -17,22 +17,6 @@
 	</head>
 
 	<body <?php body_class(); ?>>
-		<div id="page" class="hfeed site">
-			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
-			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-				</div><!-- .site-branding -->
-
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" id="toggle-button" aria-controls="primary-menu" aria-expanded="false" ><i class="fas fa-bars"></i></button>
-				<div class="toggle-menu-wrapper">
-					<input type="text" id="hamburger-search" placeholder="Search">"
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</div>
-				</nav><!-- #site-navigation -->
-			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
---
+++ themes/instanttheatre/template-parts/content-person.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Template part for displaying single posts.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
