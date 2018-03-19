5dfc00e1b33d10f376d762f01da38486f1f38067
sslnyx <sslnyx@gmail.com>
2018-03-19 10:26:37 -0700

    styled banner removed hard coding paragraph on front page

--- themes/instanttheatre/page-auditions.php
+++ themes/instanttheatre/page-auditions.php
@@ -12,9 +12,12 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
+
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
+
 			<?php endwhile; // End of the loop. ?>
+
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
