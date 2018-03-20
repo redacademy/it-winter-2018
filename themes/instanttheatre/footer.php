b649cc8c2dca14cfb725d4146358d528c0c35f1e
sslnyx <sslnyx@gmail.com>
2018-03-20 14:46:50 -0700

    finish contact page style, modify footer icons

--- themes/instanttheatre/footer.php
+++ themes/instanttheatre/footer.php
@@ -31,7 +31,7 @@
                 }
 								?>
 																<div class="social-icons">
-			<a href="#"><i class="fa fa-facebook"></i></a>
+			<a href="#"><i class="fa fa-facebook-square"></i></a>
 			<a href="#"><i class="fa fa-twitter"></i></a>
 			<a href="#"><i class="fa fa-instagram"></i></a>
 </div>
