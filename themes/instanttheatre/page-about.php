8c6e65004969028a5fbb78718e54d06f641df3cf
Brandon Liu <brandon.slipjet@gmail.com>
2018-03-20 11:28:50 -0700

    about us page mobile view styled.

--- themes/instanttheatre/page-about.php
+++ themes/instanttheatre/page-about.php
@@ -9,27 +9,35 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<div class="about-us-page">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-				<?php echo CFS() ->get( 'our_story' );?>
+					<?php get_template_part( 'template-parts/content', 'page' ); ?>
+					<div class="our-story">
+						<img src="<?php echo get_template_directory_uri();?>/assets/images/other-images/Our Story Image.jpg">
+						<div class="our-story-text">
+							<?php echo CFS() ->get( 'our_story' );?>
+						</div>
+					</div>
 
-			<?php endwhile; // End of the loop. ?>
+				<?php endwhile; // End of the loop. ?>
 
-			<h3>our school director</h3>
-			<img src="/assets/images/cast-images/nikolai_witschl_headshot_small_square-e1502153164354.jpg">
-			<!-- <div class="nikolai-text">
-				<h4>What is your improv and acting experience?</h4>
-				<p>
-				I believe that improv and acting share many of the same interconnected principles, which probably comes from the training I received in Edmonton, AB. I graduated from the BFA Acting program at the University of Alberta, while simultaneously being a cast member at Rapid Fire Theatre. After graduating I moved to Vancouver and began performing and teaching with Instant Theatre, which continues my improv career of 15+ years, where I perform in many of our weekly and monthly shows, including the Twilight Zone-esque Outer Middle Zone, and our modern take on the Bard, Shakespeare after Dark. I have also gotten work in many film and television projects, including Supernatural, Once Upon a Time, X-Files, Lucifer, Legends of Tomorrow, Supergirl (with Kevin Smith) and Guillermo Del Toro’s The Strain. 
-				</p>
-				<h4>What is your improv and acting experience?</h4>
-				<p>
-				I believe that improv and acting share many of the same interconnected principles, which probably comes from the training I received in Edmonton, AB. I graduated from the BFA Acting program at the University of Alberta, while simultaneously being a cast member at Rapid Fire Theatre. After graduating I moved to Vancouver and began performing and teaching with Instant Theatre, which continues my improv career of 15+ years, where I perform in many of our weekly and monthly shows, including the Twilight Zone-esque Outer Middle Zone, and our modern take on the Bard, Shakespeare after Dark. I have also gotten work in many film and television projects, including Supernatural, Once Upon a Time, X-Files, Lucifer, Legends of Tomorrow, Supergirl (with Kevin Smith) and Guillermo Del Toro’s The Strain. 
-				</p>
-			</div> -->
+				<h1>our school director</h1>
+				<img src="<?php echo get_template_directory_uri();?>/assets/images/cast-images/nikolai_witschl_headshot_small_square-e1502153164354.jpg"class="nikolai-pic">
+				<div class="nikolai-experience">
+					<div class="improv-experience">
+						<h2>improv experience</h2>
+						<?php echo CFS() -> get ( 'improv_experience' ); ?>
+					</div>
+					<div class="acting-experience">
+						<h2>acting experience</h2>
+						<?php echo CFS() -> get ( 'acting_experience' ); ?>
+					</div>
+				</div>
 
+				<a href="http://www.imdb.com/name/nm5775621/"><button id="imdb-button"class="blue-btn">imdb page</button></a>
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
