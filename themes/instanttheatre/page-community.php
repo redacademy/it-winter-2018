c1a36885a5a2a86a947c1759ccdd2c608a84c93f
Brandon Liu <brandon.slipjet@gmail.com>
2018-03-21 09:17:59 -0700

    added flickity initialization to community page and margin on header desktop.

--- themes/instanttheatre/page-community.php
+++ themes/instanttheatre/page-community.php
@@ -12,12 +12,22 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+        <?php get_template_part( 'template-parts/content', 'page' ); ?>
+        <div class="our-community">
+          <div class="quote-entry">
+            <p><?php echo CFS() -> get ('entry_quote'); ?></p>
+            <p><?php echo CFS() -> get ('author_of_quote'); ?></p>
+          </div>
+          <p><?php echo CFS() -> get ('our_community'); ?></p>
+        </div>
 
-			<?php endwhile; // End of the loop. ?>
+      <?php endwhile; // End of the loop. ?>
+      <div class="havana-theatre">
+        <h1>havana theatre</h1>
+        <p>Located right in the heart of Vancouver’s famous Commercial Drive (1212 Commercial Drive), Havana offers a professional theatre space nestled in the back of a fantastic Cuban restaurant. Offering dinner, drinks, and a show, Havana is the ideal location for a night out on the town. With an award-winning kitchen and fantastic patio, Instant is proud to compliment your evening with our Sunday night performances.</p>
+      </div>
 
-			 <script src ="https://form.jotform.com/static/feedback2.js" type="text/javascript"></script><script type="text/javascript"> var JFL_80118057984260 = new JotformFeedback({ formId: '80118057984260', base: 'https://form.jotform.com/', windowTitle: 'Studio Booking Enquiry', background: '#FFA500', fontColor: '#FFFFFF', type: 'false', height: 500, width: 700, openOnLoad: false }); </script> <a class="btn lightbox-80118057984260" style="margin-top: 16px"> Studio Booking Enquiry </a>
-
+      <!-- cfs get in variable - foreach -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
