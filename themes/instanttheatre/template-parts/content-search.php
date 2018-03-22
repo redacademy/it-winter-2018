a44d41c0643919f9f4353692614cb9006f710009
sslnyx <sslnyx@gmail.com>
2018-03-22 14:59:52 -0700

    thumbnail for content-search.php

--- themes/instanttheatre/template-parts/content-search.php
+++ themes/instanttheatre/template-parts/content-search.php
@@ -9,6 +9,10 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+		<?php if ( has_post_thumbnail() ) : ?>
+			<?php the_post_thumbnail( 'large' ); ?>
+		<?php endif; ?>
+
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' === get_post_type() ) : ?>
