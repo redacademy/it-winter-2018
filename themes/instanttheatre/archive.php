5afe822251b265f2d3e1e9ebca863194e07a3159
sslnyx <sslnyx@gmail.com>
2018-03-22 16:36:38 -0700

    added Archive tags page

--- themes/instanttheatre/archive.php
+++ themes/instanttheatre/archive.php
@@ -20,13 +20,44 @@ get_header(); ?>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
+<div class="shows-grid">
+<ul class="tribe-events-grid-view show-events-page">
+
 			<?php while ( have_posts() ) : the_post(); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
+	<li class="show-item">
 
+	<div class="tribe-events-grid-thumbnail">
+		<?php if ( has_post_thumbnail() ) : ?>
+		<?php the_post_thumbnail( 'large' ) ?>
+		<?php endif; ?>
+		</div>
+
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+
+		<div class="related-events-time-info">
+
+					<p><?php echo tribe_get_start_date( $post, true, 'l, F j' ); ?></p>
+						 <p><?php echo tribe_get_start_date( $post, true, 'h:i A' ); ?></p>
+						 </div>
+
+						 <div class="related-events-venue-info">
+						 <?php	
+				echo tribe_get_venue() . ' | ' . tribe_get_formatted_cost();
+		?>
+		</div>
+		<div class="related-events-links">
+				<a href="<?php echo tribe_get_event_website_url( $post ); ?>" class="buy-tickets-button">Buy Tickets</a>
+					 <a href="<?php echo tribe_get_event_link( $post ); ?>" class="learn-more-link" rel="bookmark">Learn More</a>
+					 </div>
+</li>
+
+</article><!-- #post-## -->
 			<?php endwhile; ?>
+</ul>
+			</div>
 
 			<?php the_posts_navigation(); ?>
 
