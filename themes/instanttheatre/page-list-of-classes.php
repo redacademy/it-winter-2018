96d19a881767117f31af90a8f63728e21cf3eef2
Guy Chappell <mettcat@gmail.com>
2018-03-24 18:13:15 -0700

    Classes now link to class pages (instead of EventBrite). Calendar view added to List of Classes page

--- themes/instanttheatre/page-list-of-classes.php
+++ themes/instanttheatre/page-list-of-classes.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all pages.
+ * Page Template: List of Classes
  *
  * @package RED_Starter_Theme
  */
@@ -11,113 +11,124 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<header class="page-header">
 				<h1 class='main-heading'>List of Classes</h1>
-				<ul>
-					<li>List View</li>
-					<li>Calendar View</li>
+				<ul class="view-switch">
+					<li id="list-view" class= "selected">List View</li>
+					<li id="calendar-view">Calendar View</li>
 				</ul>
 			</header>
+			<div id="class-calendar">
+				<?php echo do_shortcode('[tribe_events view="month" category="classes tribe-bar="false""]'); ?>
+			</div>
 
-			<?php /* Retrieve Class Type Loop */ ?>
+			<!-- Retrieve terms from class_type taxonomy -->
 			<?php
 				$args = array(
 									'taxonomy' => 'class_type',
 									'hide_empty' => 0
 									);
 				$terms = get_terms( $args );
-				usort( $terms, function($a, $b) {
+				usort( $terms, function($a, $b) { //Sort value 'Other' to last position of term array
 					return (($a < $b) && $a !== 'other') ? -1 : 1;
 				});
 			?>
-			<?php foreach ( $terms as $term ) : ?>
-				<div class="schedule-header">
-					<div class="schedule-header-logo">
-						<img src="<?php echo get_template_directory_uri() . '/assets/logo/rocket/rocket-' . $term->slug; ?>.svg" alt="<?php echo $term->name . ' category'; ?>"/>
-						<h2 class="featured"><?php echo $term->slug; ?></h2>
-					</div>
-					<div class="schedule-header-text">
-						<h2><?php echo $term->name; ?> Classes</h2>
-						<p><?php echo $term->description; ?></p>
-					</div>
-				</div>
-			
-				<?php
-					$args = array(
-						'tax_query' => array(
-								array(
-										'taxonomy' => 'class_type',
-										'field' => 'slug',
-										'terms' => $term->slug
-								)
-						)
-					);
-				?>	
 
-				<?php // The Query ?>
-				<?php $classes = new WP_Query( $args ); ?>
+			<!-- Output schedule for each class type -->
+			<!-- Section header -->
+			<section id="class-list">
+				<?php foreach ( $terms as $term ) : ?>
+					<div class="schedule-header">
+						<div class="schedule-header-logo">
+							<img src="<?php echo get_template_directory_uri() . '/assets/logo/rocket/rocket-' . $term->slug; ?>.svg" alt="<?php echo $term->name . ' category'; ?>"/>
+							<h2 class="featured"><?php echo $term->slug; ?></h2>
+						</div>
+						<div class="schedule-header-text">
+							<h2><?php echo $term->name; ?> Classes</h2>
+							<p><?php echo $term->description; ?></p>
+						</div>
+					</div>
+					
+					<!-- Fetch posts for each class type -->
+					<?php
+						$args = array(
+							'tax_query' => array(
+									array(
+											'taxonomy' => 'class_type',
+											'field' => 'slug',
+											'terms' => $term->slug
+									)
+							)
+						);
+					?>
+					<?php $classes = new WP_Query( $args ); ?>
 
-				<?php // The Loop ?>
-				<?php if ( $classes->have_posts() ) : ?>
-					<ul id="<?php echo $term->slug ?>" class="class-schedule">
-						<li>
-							<p class="cell">Class</p>
-							<p class="cell">Date</p>
-							<p class="cell">Time</p>
-							<p class="cell">Prerequisite</p>
-							<p class="cell">Instructor</p>
-							<p class="cell">Price</p>
-							<p class="cell">Register</p>
-						</li>
-						<?php while ( $classes->have_posts() ) : $classes->the_post(); ?>
-						<?php //Retrieve Events Calendar Pro data
-							$event_day = tribe_get_start_date( $post->ID, false, 'D, M  jS', null );
-							$event_start_time = tribe_get_start_time( $post->ID, 'g:iA' );
-							$event_end_time = tribe_get_end_time( $post->ID, 'g:iA' );
-							$event_cost = tribe_get_cost( $post->ID );
-							// $event_link = tribe_get_event_website_link( null, 'Class Details' );
-							$event_link = tribe_get_event_website_url( $post->ID );
-						?>
-						<?php //Retrieve Custom Fields data
-							$prerequisite = CFS()->get('prerequisite', false );
-							$instructors = CFS()->get( 'people' );
-						?>
+					<!-- Output class schedule table -->
+					<?php if ( $classes->have_posts() ) : ?>
+						<ul id="<?php echo $term->slug ?>" class="class-schedule">
 							<li>
-								<p class="cell"><?php echo $post->post_title; ?></p>
-								<p class="cell"><?php echo $event_day; ?></p>
-								<p class="cell">
-									<?php 
-										if( $event_start_time !== $event_end_time ) {
-											echo $event_start_time.' - '.$event_end_time;
-										 }
-										 else {
-											echo $event_start_time;
-										 } 
-									?>
-								</p>
-								<p class="cell"><?php echo $prerequisite; ?></p>
-								<div class="cell">
-									<?php if ( isset($instructors) ) : ?>
-										<?php foreach( $instructors as $post_id ) : ?>
-											<?php $instructor = get_post( $post_id ); ?>
-											<?php echo '<div>' . $instructor->post_title . '</div>'; ?>
-										<?php endforeach; ?>
-									<?php endif; ?>
-								</div>
-								<p class="cell"><?php echo $event_cost; ?></p>
-								<a class="blue-btn cell" href="<?php echo $event_link ?>" target="_blank">Class Details</a>
+								<p class="cell">Class</p>
+								<p class="cell">Date</p>
+								<p class="cell">Time</p>
+								<p class="cell">Prerequisite</p>
+								<p class="cell">Instructor</p>
+								<p class="cell">Price</p>
+								<p class="cell">Register</p>
 							</li>
-						<?php endwhile; ?>	
-					</ul>
-				
-				<?php else: ?>
-					<p class="no-classes">No classes scheduled</p>
-				<?php endif; ?>
-			<?php
-				// Restore original Post Data
-				wp_reset_postdata();
-			?>
-
-			<?php endforeach; wp_reset_postdata(); ?>
-
+							<!-- Output classes into table -->
+							<?php while ( $classes->have_posts() ) : $classes->the_post(); ?>
+							<?php 
+								//Retrieve class info in Events Calendar Pro
+								$event_date = tribe_get_start_date( $post->ID, false, 'D, M  jS', null );
+								$event_start_time = tribe_get_start_time( $post->ID, 'g:iA' );
+								$event_end_time = tribe_get_end_time( $post->ID, 'g:iA' );
+								$event_cost = tribe_get_cost( $post->ID );
+							?>
+							<?php 
+								//Retrieve class info from Custom Field Suite
+								$prerequisite = CFS()->get('prerequisite', false );
+								$instructors = CFS()->get( 'people' );
+							?>
+								<li>
+									<!-- Title -->
+									<p class="cell"><?php echo $post->post_title; ?></p>
+									<!-- Date -->
+									<p class="cell"><?php echo $event_date; ?></p>
+									<!-- Start/End Time -->
+									<p class="cell">
+										<?php 
+											if( $event_start_time !== $event_end_time ) {
+												echo $event_start_time.' - '.$event_end_time;
+											}
+											else {
+												echo $event_start_time;
+											} 
+										?>
+									</p>
+									<!-- Prerequisite -->
+									<p class="cell"><?php echo $prerequisite; ?></p>
+									<!-- Instructor -->
+									<div class="cell">
+										<?php if ( isset($instructors) ) : ?>
+											<?php foreach( $instructors as $post_id ) : ?>
+												<?php $instructor = get_post( $post_id ); ?>
+												<?php echo '<div>' . $instructor->post_title . '</div>'; ?>
+											<?php endforeach; ?>
+										<?php endif; ?>
+									</div>
+									<!-- Price -->
+									<p class="cell"><?php echo $event_cost; ?></p>
+									<!-- Link to class page -->
+									<a class="blue-btn cell" href="<?php echo get_page_link(); ?>" target="_blank">Class Details</a>
+								</li>
+							<?php endwhile; ?>	
+						</ul>
+					
+					<?php else: ?>
+						<!-- If no classes found -->
+						<p class="no-classes">No classes scheduled</p>
+					<?php endif; ?>
+					<?php wp_reset_postdata(); ?>
+				<?php endforeach; wp_reset_postdata(); ?>
+			</section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
