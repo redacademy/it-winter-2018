48d0b6ed00bc115dae7aa8f65cfd0af0042dc3d9
Dakota M <dakota.paige@hotmail.com>
2018-03-26 15:49:35 -0700

    added featured image integration to page.php for default static pages to not appear broken

--- themes/instanttheatre/template-parts/content-page.php
+++ themes/instanttheatre/template-parts/content-page.php
@@ -12,6 +12,8 @@
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
+	<?php the_post_thumbnail( 'full' ); ?>
+
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php
