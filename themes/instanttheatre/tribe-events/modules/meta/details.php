5d4e240797d150c7151b7f412a9b3ec38fe0c6f3
Dakota M <dakota.paige@hotmail.com>
2018-03-25 19:44:20 -0700

    Added escaping functions to single event, shows list and related meta php files

--- themes/instanttheatre/tribe-events/modules/meta/details.php
+++ themes/instanttheatre/tribe-events/modules/meta/details.php
@@ -54,7 +54,7 @@ $website = tribe_get_event_website_link();
 <div class="tribe-events-meta-group tribe-events-meta-group-details">
 
 	<div class="buy-tickets-link-wrapper mobile-view-hidden">
-      <a href="<?php echo tribe_get_event_website_url(); ?>" class="buy-tickets-button">Buy Tickets</a>
+      <a href="<?php echo esc_url( tribe_get_event_website_url() ); ?>" class="buy-tickets-button">Buy Tickets</a>
   </div>
 	<dl>
 
