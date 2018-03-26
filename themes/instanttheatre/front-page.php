0c743e79a44b34ca044b07cfbc0bde4b53a2a30c
sslnyx <sslnyx@gmail.com>
2018-03-25 17:40:14 -0700

    removed comments on front page

--- themes/instanttheatre/front-page.php
+++ themes/instanttheatre/front-page.php
@@ -195,24 +195,12 @@ get_header();
 			<?php while ( have_posts() ) : the_post(); ?>
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
-		<?php endif; ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php the_content(); ?>
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
-		
-
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div><!-- .entry-content -->
+			</article><!-- #post-## -->
+			
 			<?php endwhile; ?>
 
 			<?php the_posts_navigation(); ?>
